c79bc21a9c1ee3b43a5747a8a1b6f27a2f2cd753
Taofeeq001 <taofeeqkolapo@gmail.com>
2023-08-19 04:28:09 -0400

    updating NPFL api

--- users/users.js
+++ users/users.js
@@ -13,38 +13,55 @@ const users = {
             club_details: "Abia Warriors Football Club is a Nigerian professional football club based in the city of Umuahia, Abia state.Between 2005 and 2010, they played under the name (Orji Uzor Kalu FC) in honor of Abia governor Orji Uzor Kalu who helped the club with state sponsorship upon promotion to the professional level. They reverted to their old name in summer of 2010 . They won promotion to the Nigeria Premier League for the first time in August 2013 after winning their division on the last day",
             players:[
                 {
-                    goalkeeper_one: "Edwin Nwakanma",
-                    goalkeeper_two: 'Tambe Charles',
-                    goalkeeper_three: 'Uche Okafor',
-                    goalkeeper_four: 'Godgift Elkanah'
+                    player_one: "Edwin Nwakanma",
+                    player_two: 'Tambe Charles',
+                    player_three: 'Uche Okafor',
+                    player_four: 'Godgift Elkanah'
                 },
                 {
                    goalkeeper:{
-                    defender_one:'Ubong Williams',
-                    defender_two:'Chinedu Ozor',
-                    defender_three: 'Jean-Parfait Ledoux (CMR)',
-                    defender_four: 'Ebetomame Oghenetega',
-                    defender_five: 'Innocent Gabriel',
-                    defender_six: 'Samson P'
+                    player_one:'Ubong Williams',
+                    player_two:'Chinedu Ozor',
+                    player_three: 'Jean-Parfait Ledoux (CMR)',
+                    player_four: 'Ebetomame Oghenetega',
+                    player_five: 'Innocent Gabriel',
+                    player_six: 'Samson P'
                    }
                 }, 
                 {
                     midfielders:{
-                        midfielder_one: 'Abdullahi Fatai',
-                        midfielder_two: 'Promise Ogu',
-                        midfielder_three: 'Keumian Guy',
-                        midfielder_four: 'Hammed Yakub',
-                        midfielder_five: 'Adekunle Adeleke',
-                        midfielder_six: 'Somtochukwu Omini',
-                        midfielder_seven: 'Okemmiri Anthony'
+                        player_one: 'Abdullahi Fatai',
+                        player_two: 'Promise Ogu',
+                        player_three: 'Keumian Guy',
+                        player_four: 'Hammed Yakub',
+                        player_five: 'Adekunle Adeleke',
+                        player_six: 'Somtochukwu Omini',
+                        player_seven: 'Okemmiri Anthony'
+                    }
+                },
+                {
+                    forwards:{
+                        player_one: "Jimoh Oni",
+                        player_two: 'Shedrack Asiegbu',
+                        player_three: 'Auta Phillip',
+                        player_four: 'Atilola Abdulsalam Tunde',
+                        player_five: 'Odeh James',
+                        player_six: 'Ikedinachi Nwakanma'
+                    }
+                }, 
+                {
+                    others:{
+                        player_one:"Emmanuel Ugwuka",
+                        player_two: 'Obaroakpo A',
+                        player_three: 'Paul Samson',
+                        player_four: 'Ugochukwu Anozie'
                     }
                 }
-
-
                 
             ]
 
-        }
+        },
+        
     ]
 }
 module.exports = users
