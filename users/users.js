d864f522ee3b8146207082d07906057718c353af
Taofeeq001 <taofeeqkolapo@gmail.com>
2023-08-19 05:04:59 -0400

    updating NPFL clubs

--- users/users.js
+++ users/users.js
@@ -14,49 +14,49 @@ const users = {
             players:[
                 {
                     goalkeepers:{
-                        player_one: "Edwin Nwakanma",
-                        player_two: 'Tambe Charles',
-                        player_three: 'Uche Okafor',
-                        player_four: 'Godgift Elkanah'
+                        player: "Edwin Nwakanma",
+                        player: 'Tambe Charles',
+                        player: 'Uche Okafor',
+                        player: 'Godgift Elkanah'
                     }
                 },
                 {
                    defenders:{
-                    player_one:'Ubong Williams',
-                    player_two:'Chinedu Ozor',
-                    player_three: 'Jean-Parfait Ledoux (CMR)',
-                    player_four: 'Ebetomame Oghenetega',
-                    player_five: 'Innocent Gabriel',
-                    player_six: 'Samson P'
+                    player:'Ubong Williams',
+                    player:'Chinedu Ozor',
+                    player: 'Jean-Parfait Ledoux (CMR)',
+                    player: 'Ebetomame Oghenetega',
+                    player: 'Innocent Gabriel',
+                    player: 'Samson P'
                    }
                 }, 
                 {
                     midfielders:{
-                        player_one: 'Abdullahi Fatai',
-                        player_two: 'Promise Ogu',
-                        player_three: 'Keumian Guy',
-                        player_four: 'Hammed Yakub',
-                        player_five: 'Adekunle Adeleke',
-                        player_six: 'Somtochukwu Omini',
-                        player_seven: 'Okemmiri Anthony'
+                        player: 'Abdullahi Fatai',
+                        player: 'Promise Ogu',
+                        player: 'Keumian Guy',
+                        player: 'Hammed Yakub',
+                        player: 'Adekunle Adeleke',
+                        player: 'Somtochukwu Omini',
+                        player: 'Okemmiri Anthony'
                     }
                 },
                 {
                     forwards:{
-                        player_one: "Jimoh Oni",
-                        player_two: 'Shedrack Asiegbu',
-                        player_three: 'Auta Phillip',
-                        player_four: 'Atilola Abdulsalam Tunde',
-                        player_five: 'Odeh James',
-                        player_six: 'Ikedinachi Nwakanma'
+                        player: "Jimoh Oni",
+                        player: 'Shedrack Asiegbu',
+                        player: 'Auta Phillip',
+                        player: 'Atilola Abdulsalam Tunde',
+                        player: 'Odeh James',
+                        player: 'Ikedinachi Nwakanma'
                     }
                 }, 
                 {
                     others:{
-                        player_one:"Emmanuel Ugwuka",
-                        player_two: 'Obaroakpo A',
-                        player_three: 'Paul Samson',
-                        player_four: 'Ugochukwu Anozie'
+                        player:"Emmanuel Ugwuka",
+                        player: 'Obaroakpo A',
+                        player: 'Paul Samson',
+                        player: 'Ugochukwu Anozie'
                     }
                 }
                 
@@ -78,17 +78,17 @@ const users = {
             players:[
                 {
                     notable_players:{
-                        player_one:'Alalade Wasiu',
-                        player_two: 'Adeshina Gata',
-                        player_three:'Adeleye Tobi Joshua',
-                        player_four: 'Abdulrahman Bashir',
-                        player_five: 'Lordson Ichull',
-                        player_six: 'Effiom Otu Bassey',
-                        player_seven:'Etor Daniel',
-                        player_eight: 'Monday Samuel',
-                        player_nine: 'Thomas Zenke',
-                        player_ten: 'Abdullahi Oyedele',
-                        player_eleven: 'Pascal Chimdindu Anorue'
+                        player:'Alalade Wasiu',
+                        player: 'Adeshina Gata',
+                        player:'Adeleye Tobi Joshua',
+                        player: 'Abdulrahman Bashir',
+                        player: 'Lordson Ichull',
+                        player: 'Effiom Otu Bassey',
+                        player:'Etor Daniel',
+                        player: 'Monday Samuel',
+                        player: 'Thomas Zenke',
+                        player: 'Abdullahi Oyedele',
+                        player: 'Pascal Chimdindu Anorue'
                     }
                 }
             ]
@@ -101,13 +101,67 @@ const users = {
             stadium_capacity: '18,000',
             league: 'Nigeria Professional Football League',
             team_manager: 'Suleiman Abubakar',
-            club_details:' Abuja FC (also known as EFCC FC due to sponsorship reasons) is a Nigerian football club in the Nigeria National League. The club was founded in Kano, and moved to the Federal capital Territory, Abuja. It plays its home matches at the Old Parade Ground, Abuja.They were founded in 1998 as Buffalo FC by Luc Lagouche, a French teacher at the French School in Kano from September 1995 to June 2003 in order to "use sport as a factor for social integration and personal responsibility". The club was promoted out of the amateur ranks within three years after beginning play against other teams.Buffalo moved to the capital in 2006, and was renamed "F.C. Abuja" after sponsorship from the Economic and Financial Crimes Commission. They finished 5th in their division in the 2006/07 season with 3 wins, 8 draws and 3 losses and won their first of consecutive Federal Capital Territory Cups. The team won promotion to the Nigeria Premier League after placing second in the 2007–08 Division 1A. For the 2008/09 season they played home games in Kano and Minna as the Parade Ground was determined to be not up to Premier League standards. Despite being a respectable 13th place at the halfway mark, they were sent down with two games remaining with a final record of 8 wins 13 draws and 17 losses. After four years in the second division, the club was relegated to the Nationwide League in 2014. They were promoted back to the second level on the last matchday in 2016 after a 2–0 win over FRSC.';
+            club_details:'Abuja FC (also known as EFCC FC due to sponsorship reasons) is a Nigerian football club in the Nigeria National League. The club was founded in Kano, and moved to the Federal capital Territory, Abuja. It plays its home matches at the Old Parade Ground, Abuja.They were founded in 1998 as Buffalo FC by Luc Lagouche, a French teacher at the French School in Kano from September 1995 to June 2003 in order to "use sport as a factor for social integration and personal responsibility". The club was promoted out of the amateur ranks within three years after beginning play against other teams.Buffalo moved to the capital in 2006, and was renamed "F.C. Abuja" after sponsorship from the Economic and Financial Crimes Commission. They finished 5th in their division in the 2006/07 season with 3 wins, 8 draws and 3 losses and won their first of consecutive Federal Capital Territory Cups. The team won promotion to the Nigeria Premier League after placing second in the 2007–08 Division 1A. For the 2008/09 season they played home games in Kano and Minna as the Parade Ground was determined to be not up to Premier League standards. Despite being a respectable 13th place at the halfway mark, they were sent down with two games remaining with a final record of 8 wins 13 draws and 17 losses. After four years in the second division, the club was relegated to the Nationwide League in 2014. They were promoted back to the second level on the last matchday in 2016 after a 2–0 win over FRSC.',
             players: [
                 {
                     players:'No player details found'
                 }
             ]
             
+        },
+        {
+            club_name: "African Continental Bank Football Club",
+            founded: '1998 (as Buffalo F.C.)',
+            ground: '  Onikan Stadium lagos',
+            league: 'Nigeria Professional Football League',
+            team_manager: 'NIL',
+            club_details:'African Continental Bank Football Club or simply ACB FC was a Nigerian football team based in Lagos. It was sponsored by the African Continental Bank and was a founding member of the Nigerian Premier League in 1972. They were relegated from the top league for good in 1994 with a record of two wins, 12 ties and 16 losses',
+            players: [
+                {
+                    players:'No player details found'
+                }
+            ]
+        },
+        {
+            club_name: "Adamawa United Football Club",
+            founded: '2003',
+            chairman: 'Emanuel Zira',
+            ground: 'Ribadu Square Stadium, Yola Nigeria',
+            stadium_capacity: '5,000',
+            league: 'Nigeria Professional Football League',
+            club_details:'Adamawa United is a Nigerian football club from Yola, they play in the second tier of Nigeria football league system, the Nigeria National League.They play at the Ribadu Square Stadium, with a capacity of 5,000. They were relegated from the Nigerian Professional Football League in 2007 after a one-year stint. They were relegated from the league in 2012 but bought back the slot from promoted rivals Makwada F.C.',
+            players: [
+                {
+                    goalkeepers:{
+                        player: 'Victor Philemon'
+                    },
+                    defenders:{
+                        player: 'Etete Okon',
+                        player: 'Emmanuel Ogbugu',
+                        player: 'Mbai Aminu',
+                    },
+                    others:{
+                        player: 'Emmanuel Ugwuka',
+                        player: 'Lukman Surajo',
+                        player: 'Nanushi Daniel',
+                        player: 'Abba Haruna',
+                        player: 'Sale Mohammed',
+                        player: 'Adamu Yakubu',
+                        player: 'Michael Burate',
+                        player: 'Isa Garba',
+                        player: 'Sule Idris Aloma',
+                        player: 'Harmony Opukeme Ongolo',
+                        player: 'Sadiq Lawal',
+                        player: 'Stanley Nnenna',
+                        player: 'Abubakar Jibrin',
+                        player: 'Idris Abubakar',
+                        player: 'Emmanuel Ibekwe',
+                        player: 'Uche Nwokeji',
+                        
+                    }
+                    
+                }
+            ]
         }
     ]
 }
